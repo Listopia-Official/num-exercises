2cd9f920d01071c430d18e6941ab4f3e56616ab1
Listopia-Official <Listopia-Official@users.noreply.github.com>
2019-11-26 15:41:31 +0100

    Final version of ueb5

--- ueb5/newton.py
+++ ueb5/newton.py
@@ -6,47 +6,90 @@ the fifth exercise (first programming exercise) on the second worksheet of Numer
 import numpy as np
 import matplotlib.pyplot as plt
 
-def divDiff(points): # points: An array containing the f_i with key x_i
-    pointCount = len(points)
+"""
+Computes the divided differences for the points supplied to the function,
+and returns the coefficients for the interpolation polynome with the Newton base.
+
+For that, it creates and stores a scheme simliar to that specified in the lecture, but
+mirrorwd and turned, so that the first row are the coefficients.
+
+Input: An array containing the points (x_i, f_i)
+Output: An array containing the coefficients c_0, ..., c_k
+"""
+def newtonCoefficients(points):
+    pointCount = len(points) # Helper variable
     
-    pointMap = {point[0]:point[1] for point in points}
+    pointMap = {point[0]:point[1] for point in points} # A map containing the x_i as key and the f_i as value
     
-    diffMatrix = np.zeros(shape=(pointCount,pointCount))
+    knownDiffs = {} # Empty map where the known computed, divided differences are stored
     
-    knownDiffs = {}
-    
-    c = []
+    c = [] # The coefficient list
     
     for i in range(pointCount):
-        for j in range(pointCount-i):
-            arg = tuple([points[k+j][0] for k in range(i+1)])
-            diff2 = diff(pointMap, arg, knownDiffs)
-            knownDiffs[arg] = diff2
-            diffMatrix[i][j] = diff2
+        for j in range(pointCount-i): # The scheme is in triangular form, so we don't need to iterate through the whole square
+            
+            arg = tuple([points[k+j][0] for k in range(i+1)]) # Compute the argument [x_k, ..., x_j] of the divided difference
+            
+            dividedDifference = diff(pointMap, arg, knownDiffs) # Compute the divided difference
+            knownDiffs[arg] = dividedDifference # Add the difference to the map of known ones
             
             if j is 0:
-                c.append(diff2)
+                c.append(dividedDifference) # If we're on the first column, we have a coefficient c_i - store that
             
     return c
 
+"""
+A helper function which computes the divided difference f[x_k, ..., x_j] like specified in the lecture.
+
+Input: 
+    pointMap: A map containing the x_i as key and the f_i as value
+    arg: A list containing the x_k, ..., x_j (the argument of the divided difference)
+    knownDiffs: A map containing the argument [x_i, ..., x_l] as key and the divided difference f[x_i, ..., x_l] as value
+
+Output:
+    The value of the divided difference (a scalar value).
+"""
 def diff(pointMap, arg, knownDiffs):
-    if(len(arg) == 1):
-        return pointMap[arg[0]]
-    else:
+    if(len(arg) == 1): # One argument
+        return pointMap[arg[0]] # f[x_i] = f_i
+    else: # At least two arguments
+        """
+        Computes f[x_(k+1), ..., x_j] - f[x_k, ..., x_(j-1)] / (x_j - x_k)
+        """
         return (knownDiffs[arg[1:len(arg)]] - knownDiffs[arg[:len(arg)-1]])/(arg[len(arg)-1] - arg[0])
     
+"""
+Computes the Newton base polynome functions as specified on the lecture.
+
+Input:
+    points: An array containing the points relevant for the base
+    
+Output:
+    An array containing the Newton base functions as lambdas
+"""
 def newtonBase(points):
     base = []
     for j in range(len(points)):
         if j == 0:
-            base.append(lambda x: 1)
+            base.append(lambda x: 1) # The 0-th base is p(x) = 1
         else:
+            """
+                p(x) = product from k=0 to j-1 of (x-x_k), as specified in lecture
+            """
             base.append(lambda x, j=j, points=points: np.prod(np.array([x-points[i][0] for i in range(j)])))
     
     return base
     
-def plotSolution(points, base, solution):    
-    minX = min([points[i][0] for i in range(len(points))])
+"""
+Plots the interpolatio polynome and the specified points.
+
+Input:
+    points: The points to plot (x_i, f_i)
+    base: The Newton base
+    coefficients: The corresponding Newton coefficients
+"""
+def plotSolution(points, base, coefficients):    
+    minX = min([points[i][0] for i in range(len(points))]) # Compute plot area from points
     maxX = max([points[i][0] for i in range(len(points))])
     
     for point in points:
@@ -54,16 +97,21 @@ def plotSolution(points, base, solution):
         
     x = np.linspace(minX, maxX, 1000) # x-Axis
     
-    polynome = lambda x : sum([base[i](x) * solution[i] for i in range(len(solution))])
+    polynome = lambda x : sum([base[i](x) * coefficients[i] for i in range(len(coefficients))]) # Assemble the interpolation polynome
         
-    plt.plot(x, [polynome(x_comp) for x_comp in x]) # Display the interpolation
+    plt.plot(x, [polynome(x_comp) for x_comp in x]) # Display the interpolation polynome
     plt.show()
     
-points = [(-1, -1), (0,0), (1,3)] # Interpolation constraints
+points = [(-1, -1), (0,0), (1,3)] # Interpolation constraints as in b)
 base = newtonBase(points) # The Newton base
-coefficients = divDiff(points) # Coefficients of the Newton base
+coefficients = newtonCoefficients(points) # Coefficients of the Newton base
 
 print("Points:",points)
-print("Newton Base Coefficients:",coefficients)
+print("Newton Base Coefficients:",coefficients) # a)
 
-plotSolution(points, base, coefficients)
+plotSolution(points, base, coefficients) # b)
+
+"""
+Comparison with Abb 4.3 of the script:
+    Both plots display the same polynome, however we plot here with the smallest possible definition area.
+"""
